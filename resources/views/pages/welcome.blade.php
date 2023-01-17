15d7b371507115c2cc2bdb055a9b03847cffd458
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2023-01-17 14:28:46 +0000

    Fatto =)

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts.app')
-@section('main-content')
-    <h1>Tutte le case</h1>
 
-    {{-- @foreach ($houses as $elem )
-        <span>{{$elem->city}}</span>
-    @endforeach --}}
+@section('main-content')
+    <h1 class="text-center text-danger my-2">BENVENUTI</h1>
+
+    <table class="table table-bordered w-75 m-auto">
+        <thead>
+            <tr>
+                <th scope="col">Arrivi</th>
+                <th scope="col">Partenze</th>
+                <th scope="col">Destinazione</th>
+                <th scope="col">Binario</th>
+                <th scope="col">Prezzo</th>
+            </tr>
+        </thead>
+        <tbody>
+    @foreach ($trains as $elem)
+
+        <tr>
+            <td>{{ $elem->arrivi }}</td>
+            <td>{{ $elem->partenze }}</td>
+            <td>{{ $elem->destinazione }}</td>
+            <td>{{ $elem->binario }}</td>
+            <td>{{ $elem->price }} €</td>
+        </tr>
+
+    @endforeach
+</tbody>
+</table>
 
 @endsection
